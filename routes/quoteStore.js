c7ee547f012381df7a1dd0815c8fdc49381b7c05
jihongzuo <zuojihong@hotmail.com>
2015-01-09 20:09:50 +0800

    add webworker module

---
+++ routes/quoteStore.js
@@ -0,0 +1,23 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var QuoteSchema = new Schema({
+    code: String,
+    time: Date,
+    price: Number    
+});
+mongoose.model('Quote', QuoteSchema, 'stocks');
+
+exports.getLatestQuoteByCode = function(req, res) {
+    var model = mongoose.model('Quote');
+    var code = req.params.code;
+    var query = model.find({ code: code }).sort('-time').limit(7);
+    query.exec(function(err, quotes) {
+        console.log('getLatestByCode returned');
+        if (!err) {
+            res.send({status: 0, data: quotes});
+        } else {
+            console.log({status: -1, data: null});
+        }
+    });
+} 
